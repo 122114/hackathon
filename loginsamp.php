863f6887e96624792e61a19ee8f7d2a6867c4766
llewellynho <ho.llewellyn@gmail.com>
2015-02-19 11:42:19 +0800

    withlogona

--- loginsamp.php
+++ loginsamp.php
@@ -29,7 +29,7 @@
 <nav class="navbar navbar-default navbar-static-top">
   <div class="container-fluid">
  	<div class="row">
- 		 <div class="col-md-5"><h1>World Be Green</h1></div>
+ 		 <div class="col-md-5"><img src = "finaltitle.jpg"/></div>
  		 <br>
     	 <div class="col-md-7"><form class="form-inline pull-right">
   <div class="form-group">
