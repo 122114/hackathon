62e714f44ecd2e1100f0f2ff26714b44bdb013d7
llewellynho <ho.llewellyn@gmail.com>
2015-02-19 11:26:38 +0800

    wapanahuman

---
+++ about.php
@@ -0,0 +1,77 @@
+<html>
+<title>Sample</title>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	
+	<link rel="stylesheet" href="css/bootstrap.css"/>
+	<link rel="stylesheet" href="css/newstyle.css"/>
+</head>
+	
+<body>
+	
+	<script src="javas.js"></script>
+	<script src="bootstrap.min.js"></script>
+
+
+<nav class="navbar navbar-default navbar-static-top">
+  <div class="container-fluid">
+ 	<div class="row">
+ 		 <div class="col-md-5"><img src = "finaltitle.jpg"/></div>
+ 		 <br>
+    	 <div class="col-md-7">
+          <div class="usernamegoes pull-right">
+              <!--Put username here-->
+              <center>
+              <img src="" width="30" height="30"><h4>Welcome My UserName</h4>
+              </center>
+          </div>
+      </div>
+    <div>
+  </div>
+</nav>
+<nav class="navbar2 navbar-inverse">
+  <div class="container-fluid">
+    <div>
+      <ul class="nav navbar-nav">
+        <li ><a href="#">Home</a></li>
+        <li class="active"><a href="#">About Us</a></li>
+        <li><a href="#">Benefeciaries</a></li> 
+        <li><a href="#">Event</a></li> 
+      </ul>
+    </div>
+  </div>
+</nav>
+
+	<div class="rows">
+	<div class="col-md-12">
+	
+		<div class="aboutus">
+		<h1>Never doubt that a small group of thoughtful, 
+		committed citizens can change the world. Indeed, it is the only thing that ever has</h1>
+    </div>
+	
+	</div>
+		
+	
+
+      
+    </div>
+  </div>
+	</div>
+</div>
+</div>
+<div class="foot">
+  <div class="col-md-12">
+    <h5>Brought to you by Microtron </h5>
+  </div>
+</div>
+
+	
+		
+	
+
+
+
+</body>
+</html>
