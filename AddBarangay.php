c42f80d5d2b4c93ff89c5b757f23aa1f717a1c1c
llewellynho <ho.llewellyn@gmail.com>
2015-02-19 13:45:42 +0800

    withlinkandarrangements

---
+++ AddBarangay.php
@@ -0,0 +1,119 @@
+<html>
+<title>Sample</title>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	
+	<link rel="stylesheet" href="css/bootstrap.css"/>
+	<link rel="stylesheet" href="css/newstyle.css"/>
+</head>
+	
+<body>
+	
+	<script src="javas.js"></script>
+	<script src="bootstrap.min.js"></script>
+
+
+<nav class="navbar navbar-default navbar-static-top">
+  <div class="container-fluid">
+ 	<div class="row">
+ 		 <div class="col-md-5"><img src = "finaltitle.jpg"/></div>
+ 		 <br>
+    	 <div class="col-md-7">
+          <div class="usernamegoes pull-right">
+              <!--Put username here-->
+              <center>
+              <img src="" width="30" height="30"><h4>Welcome My UserName</h4>
+              </center>
+          </div>
+      </div>
+    <div>
+  </div>
+</nav>
+<nav class="navbar2 navbar-inverse">
+  <div class="container-fluid">
+    <div>
+      <ul class="nav navbar-nav">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Events</a></li>
+        <li><a href="#">Sponsors</a></li> 
+        <li><a href="#">Tree Planters</a></li>
+         <li class="active"><a href="AddBarangay.php">Barangays</a></li> 
+          <li><a href="#">Settigs</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+	<div class="rows">
+  <div class="contain">
+	<div class="col-md-12">
+	 
+		<div class="col-md-2">
+    <br>
+      <ul class="nav nav-pills nav-stacked">
+        <li role="presentation" class="active"><a href="AddBarangay.php">Add Barangay</a></li>
+        <li role="presentation"><a href="Confirmrequests.php">Confirm requests</a></li>
+
+      </ul>   
+    </div>
+    <div class="contain">
+    <div class="col-md-2">
+    <div class="addbar">
+        <div class="form-group">
+          <br>
+          <label for="exampleInputName2">Barangay</label>
+          <input type="text" class="form-control left-align" id="exampleInputName2">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail2">Location</label>
+          <input type="password" class="form-control" id="exampleInputEmail2">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail2">Email</label>
+          <input type="password" class="form-control" id="exampleInputEmail2">
+        </div>
+
+  <button type="submit" class="btn btn-default">Add</button>
+  </div>
+
+	</div>
+  </div>
+  <div class="col-md-8">
+  <div class="listbar">
+      <!--lists of barangays-->
+      <br>
+      <h2 class=>Lists Of Barangays</h2>
+        <ul class="list-group">
+          <li class="list-group-item">Cras justo odio</li>
+          <li class="list-group-item">Dapibus ac facilisis in</li>
+          <li class="list-group-item">Morbi leo risus</li>
+          <li class="list-group-item">Porta ac consectetur ac</li>
+          <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+    </div>
+  </div>
+	</div>
+	
+	
+
+      
+    </div>
+  </div>
+	</div>
+</div>
+</div>
+<div class="foot">
+  <div class="col-md-12">
+    <h5>Brought to you by Microtron </h5>
+  </div>
+</div>
+
+	
+		
+	
+
+
+
+</body>
+</html>
---
+++ Benefeciaries.php
@@ -0,0 +1,82 @@
+<html>
+<title>Beneficiaries</title>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	
+	<link rel="stylesheet" href="css/bootstrap.css"/>
+	<link rel="stylesheet" href="css/newstyle.css"/>
+</head>
+	
+<body>
+	
+	<script src="javas.js"></script>
+	<script src="bootstrap.min.js"></script>
+
+
+<nav class="navbar navbar-default navbar-static-top">
+  <div class="container-fluid">
+ 	<div class="row">
+ 		 <div class="col-md-5"><img src = "finaltitle.jpg"/></div>
+ 		 <br>
+    	 <div class="col-md-7">
+          <div class="usernamegoes pull-right">
+              <!--Put username here-->
+              <center>
+              <img src="" width="30" height="30"><h4>Welcome My UserName</h4>
+              </center>
+          </div>
+      </div>
+    <div>
+  </div>
+</nav>
+<nav class="navbar2 navbar-inverse">
+  <div class="container-fluid">
+    <div>
+      <ul class="nav navbar-nav">
+        <li><a href="loginsamp.php">Home</a></li>
+        <li ><a href="about.php">About Us</a></li>
+        <li class="active"><a href="Benefeciaries.php">Benefeciaries</a></li> 
+        <li><a href="#">Event</a></li> 
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="rows">
+	<div class="col-md-12">
+	
+		<div class="col-md-2">
+			<ul class="nav nav-pills nav-stacked">
+ 				<li>Barangay1</li>
+			</ul>
+		</div>
+		<div class="col-md-2">
+
+		</div>
+	
+	</div>
+		
+	
+
+      
+    </div>
+  </div>
+	</div>
+</div>
+</div>
+
+
+<div class="foot">
+  <div class="col-md-12">
+    <h5>Brought to you by Microtron </h5>
+  </div>
+</div>
+
+	
+		
+	
+
+
+
+</body>
+</html>
